acedd2652e7144bfd8549885dd94ff3c2b18aea9
Rafet ZIOUNE <rafet.zitoune@2isa.org>
2026-08-24 23:05:56 +0200

    fin projetBanque + début Guilde

--- GestionComptesBancaires/Banque.cs
+++ GestionComptesBancaires/Banque.cs
@@ -10,7 +10,7 @@ namespace GestionComptesBancaires
         {
             string soldeStr;
             string nbcompteStr;
-            double nouveauSolde;
+            
             Banque nouveau = new Banque();
             ComptesBancaires nouveauCompte = new ComptesBancaires();
 
@@ -34,22 +34,70 @@ namespace GestionComptesBancaires
                 soldeStr = Console.ReadLine();
             }
             
-            nouveauCompte.nouveauSolde = double.Parse(soldeStr);
+            double nouveauSolde = double.Parse(soldeStr);
+            nouveauCompte.Solde = nouveauSolde;
 
             Console.WriteLine("Numero du compte : ");
-            
+            nbcompteStr = Console.ReadLine();
 
+            while(!int.TryParse(nbcompteStr, out int nouveauNumeroCompte))
+            {
+                Console.WriteLine("Saisie invalide !");
+                Console.WriteLine("Numero du compte : ");
+                nbcompteStr = Console.ReadLine();
+            }
 
+            for (int i = 0; i < ListeComptes.Count; i++)
+            {
+                if (ListeComptes[i].NumeroCompte == nouveauNumeroCompte)
+                {
+                    Console.WriteLine("Le numéro de compte existe déjà !");
+                    Console.WriteLine("Veuillez saisir un autre numéro de compte : ");
+                    nbcompteStr = Console.ReadLine();
+                    while(!int.TryParse(nbcompteStr, out nouveauNumeroCompte))
+                    {
+                        Console.WriteLine("Saisie invalide !");
+                        Console.WriteLine("Veuillez saisir un autre numéro de compte : ");
+                        nbcompteStr = Console.ReadLine();
+                    }
+                    nouveauNumeroCompte = int.Parse(nbcompteStr);
+                }
+            }
+
+            double nouveauNumeroCompte = int.Parse(nbcompteStr);
+            nouveauCompte.nouveauNumeroCompte = nouveauNumeroCompte;
+
+            ListeComptes.Add(nouveauCompte);
         }
 
         public void AfficherCompte()
         {
-            
+            Console.Clear();
+            Console.WriteLine("===== Afficher les comptes Bancaires =====");
+
+            for (int i = 0; i < ListeComptes.Count; i++)
+            {
+                Console.WriteLine("\n");
+                Console.WriteLine("Compte n°" + (i + 1) + " : ");
+                ListeComptes[i].AfficherCompte();
+            }
+
+            Console.WriteLine("\n");
+            Console.WriteLine("Nombre de comptes Bancaires : " + ListeComptes.Count);
+            Console.WriteLine("Appuyer sur une touche pour revenir au menu principal");
+            Console.ReadKey();
         }
 
-        public void CapitalTotal()
+        public void AfficherCapitalTotal()
         {
-            
+            double capitalTotal = 0;
+            for (int j = 0; j < ListeComptes.Count; j++)
+            {
+                capitalTotal = capitalTotal + ListeComptes[j].Solde;
+            }
+            Console.WriteLine("Capital total des comptes : " + capitalTotal);
+            Console.WriteLine("Appuyer sur une touche pour revenir au menu principal");
+            Console.ReadKey();
         }
     }
 }
